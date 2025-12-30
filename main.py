d9e5729714a4bbc52cc86ee7dee204b74e66e2a6
Dhruwal Kushwaha <crazydhruwal@gmail.com>
2025-12-30 11:33:44 +0530

    Fix CORS error

--- main.py
+++ main.py
@@ -445,10 +445,7 @@ app = FastAPI(title="ReportSahayak API", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    allow_origins=["*"],  # <--- CHANGED: Allows ALL domains (Vercel, Localhost, etc.)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
